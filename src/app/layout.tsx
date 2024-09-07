f877c451290a167361c406911209581ec3714fd2
NHeroOP <neeravtheheroo@gmail.com>
2024-09-08 01:52:46 +0530

    update page.tsx and layout.tsx for temporary purposes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { themeChange } from 'theme-change'
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dracula">
       <body className={inter.className}>{children}</body>
     </html>
   );
