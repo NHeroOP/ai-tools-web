fbcd08dde3e32f1a8ab812dce9e9684024e95b49
NHeroOP <neeravtheheroo@gmail.com>
2024-09-09 00:22:28 +0530

    update summarize route to use gemini

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -1,53 +1,43 @@
-import Groq from "groq-sdk";
-import { NextResponse } from "next/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
-const prompt = (text: string) => {
-  return `As a professional summarizer, create a concise and comprehensive summary of the provided text: [${text}]. Ensure the summary is detailed, capturing main ideas and essential information while eliminating extraneous language. Focus strictly on the provided text, without incorporating external information. Format the summary in paragraph form for readability. The response must be a JSON object structured as follows: { "summary": "your summary here", "title": "suitable title here" }, using double quotes for both keys and values. Ensure that all control characters, such as newlines, tabs, or other special characters, are properly escaped. For line breaks, use \\n, and for single quotes, use \\". Return only the JSON object as described, with no additional information.`;
-}
+const instruction = `You are a versatile text summarization expert capable of handling text, files, and links. Produce concise and informative summaries in JSON format: {"summary": "A detailed summary", "title": "A suitable title"}. Ensure summaries are comprehensive, capturing main ideas and eliminating extraneous language. Format summaries in paragraphs for readability. Handle various input formats: text, files (extract relevant content), links (fetch content, summarize based on URL if inaccessible). Ensure character escaping using \\n and \\". Return only the JSON object and nothing else.`
 
 export async function POST(req: Request) {
-  const { text } = await req.json()
+  const { text } = await req.json();
 
   if (!text) {
     return Response.json({
-      error: "Text is required"
-    }, {status: 400});
+      error: "Prompt is required"
+    }, { status: 400 });
   }
+   
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    systemInstruction: instruction
+  });
 
   try {
-    const res = await groq.chat.completions.create({
-      messages: [
-        {
-          role: "user",
-          content: prompt(text),
-        },
-      ],
-      model: "llama3-70b-8192",
-    });
-    
-    let content: any = res.choices[0].message.content;
-    console.log("content", content);
-    
-    
-    const jsonContent = content.match(/{[\s\S]*}/);
-    
-    if (!jsonContent) {
-      return Response.json({
-        error: "Failed to parse JSON"
-      }, {status: 500});
-    }
+    const result = await model.generateContent(text);
+    const match = result.response.text().match(/\{.*\}/)
+    const summaryStringObj = match ? match[0] : undefined;
 
-    let jsonString = jsonContent[0].trim();
+    if (!summaryStringObj) {
+      return Response.json({
+        error: "An error occurred while summarizing the text. Please try again."
+      }, { status: 500 });
+    }
+  
+    const summaryObj = JSON.parse(summaryStringObj)
     
-    const summaryObj = JSON.parse(jsonString);
-    return NextResponse.json(summaryObj, { status: 200 });
-    
+    return Response.json(summaryObj, { status: 200 });
   }
   catch (error: any) {
+    console.log(error);
+    
     return Response.json({
       error: error.message
-    }, {status: 500});
+    }, { status: 500 });
+    
   }
-  
 }
