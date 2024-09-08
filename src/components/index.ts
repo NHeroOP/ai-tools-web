9107cc38bc354bf6e34b5e79395c20a7b931fa71
NHeroOP <neeravtheheroo@gmail.com>
2024-09-08 15:37:09 +0530

    add index file in components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,11 @@
 import Navbar from "./Navbar";
+import MenuBar from "./MenuBar";
+import Footer from "./Footer";
+import ToolCard from "./ToolCard";
 
 export {
-  Navbar
+  Navbar,
+  MenuBar,
+  ToolCard,
+  Footer,
 }
